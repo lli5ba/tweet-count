40519ff825f7df9ea7c81d8f5772b054b7625829
Leandra <lli5ba@virginia.edu>
2016-04-23 23:39:30 -0400

    Added files via upload

---
+++ src/main/java/com/microsoft/example/PrintResults.java
@@ -0,0 +1,29 @@
+package storm.trident.operation.builtin;
+
+import storm.trident.operation.BaseFilter;
+import storm.trident.tuple.TridentTuple;
+import java.sql.Timestamp;
+import java.util.Date;
+
+public class PrintResults extends BaseFilter {
+	private int count;
+    public PrintResults() {
+        count = 0;
+    }
+
+
+    @Override
+    public boolean isKeep(TridentTuple tuple) {
+		if (count == 0) {
+			java.util.Date date= new java.util.Date();
+			System.out.print(new Timestamp(date.getTime()));
+		} else if (count < 10) {
+			System.out.printl( " " + tuple.toString());
+			count++;
+		} else if (count == 10) {
+			//reset count
+			count = 10;
+		}
+		return true;
+    }
+}
