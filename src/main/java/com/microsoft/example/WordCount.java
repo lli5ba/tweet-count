7dd5f2b233ebca367af127c02e6e90b7e15e26d2
Leandra <lli5ba@virginia.edu>
2016-04-27 16:02:10 -0400

    Update WordCount.java

--- src/main/java/com/microsoft/example/WordCount.java
+++ src/main/java/com/microsoft/example/WordCount.java
@@ -9,8 +9,10 @@ import backtype.storm.tuple.Values;
 import backtype.storm.tuple.Fields;
 import backtype.storm.topology.OutputFieldsDeclarer;
 
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.TreeMap;
 
 import twitter4j.HashtagEntity;
 import twitter4j.Status;
@@ -24,6 +26,8 @@ public class WordCount extends BaseFunction {
     Map<String, Integer> counts = new HashMap<String, Integer>();
     //For holding last date
     Date date = new java.util.Date();
+    //to sort TreeMap
+    ValueComparator bvc =  new ValueComparator(counts);
   @Override
   public void execute(TridentTuple tuple, TridentCollector collector) {
     //Get the word contents from the tuple
@@ -45,16 +49,46 @@ public void updateDate(){
 	Date date2 = new java.util.Date();
 	double seconds = (date2.getTime()-date.getTime())/1000;
 	if (seconds >= 5) {
-		System.out.println(new Timestamp(date2.getTime()));
-		System.out.println(counts.toString());
 		this.date = date2;
+		System.out.println(counts.toString());
+		System.out.print(new Timestamp(date2.getTime()));
+		System.out.print(": ");
+		printTopTen();
+		
+        
 	}
 	
 }
-	//Declare that we will emit a tuple containing two fields; word and count
-    //@Override
-    //public void declareOutputFields(OutputFieldsDeclarer declarer) {
-    //  declarer.declare(new Fields("hashtag", "count"));
-    //}
-  
+
+public void printTopTen() {
+	//print top 10 words (words with highest counts)
+	TreeMap<String, Integer> sorted_counts = new TreeMap<String,Integer>(bvc);
+	sorted_counts.putAll(counts);
+	int i = 0;
+	for (String s : sorted_counts.keySet()) {
+		if (i == 10) {
+			break;
+		}
+		System.out.println(s + " ");
+		i++;
+	}
+	System.out.println();
+}
+}
+
+class ValueComparator implements Comparator<String> {
+
+    Map<String, Integer> base;
+    public ValueComparator(Map<String, Integer> base) {
+        this.base = base;
+    }
+
+    // Note: this comparator imposes orderings that are inconsistent with equals.    
+    public int compare(String a, String b) {
+        if (base.get(a) >= base.get(b)) {
+            return -1;
+        } else {
+            return 1;
+        } // returning 0 would merge keys
+    }
 }
